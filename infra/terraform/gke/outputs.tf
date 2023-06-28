0077748a75c633482580a2d29269f8e3842dee5d
Manasa Kandula <manasakandula.m@gmail.com>
2023-06-28 13:17:07 +0000

    foreach bash script. WIP

--- infra/terraform/gke/outputs.tf
+++ infra/terraform/gke/outputs.tf
@@ -3,21 +3,22 @@ output "fleet-clusters" {
         for cluster in module.gke: {
         name = cluster.name
         id = cluster.cluster_id
-        endpoint = cluster.endpoint
         region = cluster.region
         zones = cluster.zones
         }
     ]
-    sensitive = true
 }
 
 output "config-clusters" {
     value = {
         name = module.gke-config-cluster.name
         id = module.gke-config-cluster.cluster_id
-        endpoint = module.gke-config-cluster.endpoint
         region = module.gke-config-cluster.region
         zones = module.gke-config-cluster.zones
         }
-    sensitive = true
 }
+
+output "num-fleet-clusters"{
+    value = length(module.gke)
+}
+

2dccac5a1b48031f11b887093c9d0620fa355eb6
Manasa Kandula <manasakandula.m@gmail.com>
2023-12-18 20:50:44 +0000

    created zone clusters json output

--- infra/terraform/platform-values/main.tf
+++ infra/terraform/platform-values/main.tf
@@ -1,11 +1,18 @@
 locals {
   clusters_info = data.terraform_remote_state.gke.outputs.fleet-clusters
-  ip_address = data.terraform_remote_state.gclb.outputs.ip_address
-
+  zone_clusters_info = {
+    for index,item in local.clusters_info:
+      item.zones[0] => item
+  }
 }
 
 resource "google_storage_bucket_object" "cluster_info" {
   name   = "platform-values/clusters.json"
   content = jsonencode(local.clusters_info)
+  bucket = var.project_id
+}
+resource "google_storage_bucket_object" "zone_cluster_info" {
+  name   = "platform-values/zone_clusters.json"
+  content = jsonencode(local.zone_clusters_info)
   bucket = var.project_id
 }
--- infra/terraform/platform-values/remote.tf
+++ infra/terraform/platform-values/remote.tf
@@ -5,11 +5,3 @@ data "terraform_remote_state" "gke" {
     prefix = "tfstate/gke"
   }
 }
-
-data "terraform_remote_state" "gclb" {
-  backend = "gcs"
-  config = {
-    bucket = var.project_id
-    prefix = "tfstate/gclb"
-  }
-}

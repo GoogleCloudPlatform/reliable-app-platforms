ae1af3fe3e275667a11f41c4236460afeffe5d18
Manasa Kandula <manasakandula.m@gmail.com>
2023-12-19 11:19:25 +0000

    updated hub

--- infra/terraform/gke/hub.tf
+++ infra/terraform/gke/hub.tf
@@ -1,12 +1,32 @@
+locals{
+  fleet-clusters =   [
+    for cluster in module.gke : {
+      name   = cluster.name
+      id     = cluster.cluster_id
+      region = cluster.region
+      zones  = cluster.zones
+    }
+  ]
+  config-clusters = [{
+    name   = module.gke-config-cluster.name
+    id     = module.gke-config-cluster.cluster_id
+    region = module.gke-config-cluster.region
+    zones  = module.gke-config-cluster.zones
+  }]
+ 
+ all_clusters = concat(local.fleet-clusters, local.config-clusters)
+
+}
+
 module "fleet-hub" {
-  for_each        = module.gke
+  for_each        = {for i,v in local.all_clusters: i=>v}
   source          = "terraform-google-modules/kubernetes-engine/google//modules/fleet-membership"
   version         = "28.0.0"
   project_id      = var.project_id
-  location        = each.value.location
+  location        = each.value.region
   cluster_name    = each.value.name
   membership_name = each.value.name
-  depends_on      = [module.gke]
+  depends_on      = [module.gke, module.gke-config-cluster]
 }
 
 module "config-hub" {
--- infra/terraform/gke/outputs.tf
+++ infra/terraform/gke/outputs.tf
@@ -1,20 +1,14 @@
-output "fleet-clusters" {
-  value = [
-    for cluster in module.gke : {
-      name   = cluster.name
-      id     = cluster.cluster_id
-      region = cluster.region
-      zones  = cluster.zones
-    }
-  ]
-}
+
 
 output "config-clusters" {
-  value = {
-    name   = module.gke-config-cluster.name
-    id     = module.gke-config-cluster.cluster_id
-    region = module.gke-config-cluster.region
-    zones  = module.gke-config-cluster.zones
-  }
+  value = local.config-clusters
+}
+
+output "fleet-clusters" {
+  value = local.fleet-clusters
+}
+
+output "all-clusters" {
+  value = local.all_clusters
 }
 

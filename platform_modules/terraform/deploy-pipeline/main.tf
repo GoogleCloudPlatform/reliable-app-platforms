93adfab9d5afb1d7337259bbceff3fd08e07068e
Manasa Kandula <manasakandula.m@gmail.com>
2024-01-04 13:03:25 +0000

    added custom service account for cloud deploy

--- platform_modules/terraform/deploy-pipeline/main.tf
+++ platform_modules/terraform/deploy-pipeline/main.tf
@@ -24,12 +24,32 @@ data "google_storage_bucket_object_content" "config_cluster_info" {
   bucket = var.project_id
 }
 
+resource "google_service_account" "clouddeploy" {
+  project = var.project_id
+  account_id   = "clouddeploy"
+  display_name = "Cloud Deploy Service Account"
+}
+
+resource "google_project_iam_member" "clouddeploy_container_developer" {
+  project = var.project_id
+  role    = "roles/container.developer"
+  member  = "serviceAccount:${google_service_account.clouddeploy.email}"
+}
+
+resource "google_project_iam_member" "clouddeploy_member_deploy_jobrunner" {
+  project = var.project_id
+  role    = "roles/clouddeploy.jobRunner"
+  member  = "serviceAccount:${google_service_account.clouddeploy.email}"
+}
 
 resource "google_clouddeploy_target" "child_target" {
   for_each = { for i, v in local.targets : i => v }
   location = var.pipeline_location
   name     = "child-target-${var.service_name}-${each.value.name}"
-
+  execution_configs {
+    usages            = ["RENDER", "DEPLOY"]
+    service_account = google_service_account.clouddeploy.email
+  }
   gke {
     cluster = each.value.id
   }
@@ -53,7 +73,10 @@ resource "google_clouddeploy_target" "multi_target" {
 resource "google_clouddeploy_target" "config_target" {
   location = var.pipeline_location
   name     = "target-config-${var.service_name}"
-
+  execution_configs {
+    usages            = ["RENDER", "DEPLOY"]
+    service_account = google_service_account.clouddeploy.email
+  }
   gke {
     cluster = local.config_cluster_info[0].id
   }

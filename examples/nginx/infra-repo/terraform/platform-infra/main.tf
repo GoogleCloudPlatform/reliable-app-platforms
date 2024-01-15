91eb8e0ffa53d89225a3f02bbc6b056cb95c0601
Manasa Kandula <manasakandula.m@gmail.com>
2024-01-15 12:42:37 +0000

    cleaned up app deploy

--- examples/nginx/infra-repo/terraform/platform-infra/main.tf
+++ examples/nginx/infra-repo/terraform/platform-infra/main.tf
@@ -19,9 +19,3 @@ module "endpoint" {
     project_id = var.project_id
     service_name = "${var.service_name}service"
 }
-
-module "slos" {
-    source = "git::https://github.com/ameer00/Building-Reliable-Platforms-on-GCP-with-Google-SRE.git//modules/slos?ref=tf_in_repo_examples"
-    project_id = var.project_id
-    service_name = "${var.service_name}service"
-}

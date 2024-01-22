169bca2ad276ebc2e3273010cff1c6e2ebf1f980
Manasa Kandula <manasakandula.m@gmail.com>
2024-01-22 11:04:03 +0000

    ar removed tag immutability

--- modules/artifact-registry/main.tf
+++ modules/artifact-registry/main.tf
@@ -5,6 +5,6 @@ resource "google_artifact_registry_repository" "repo" {
   format        = "DOCKER"
   project = var.project_id
   docker_config {
-    immutable_tags = true
+    immutable_tags = false
   }
 }

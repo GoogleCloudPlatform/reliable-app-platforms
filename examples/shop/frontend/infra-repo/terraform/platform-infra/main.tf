5d725ae91ca07c0de05d1a3b33a8fb05da17e08c
Manasa Kandula <manasakandula.m@gmail.com>
2024-01-28 10:18:59 +0000

    all services of shop deployed

--- examples/shop/frontend/infra-repo/terraform/platform-infra/main.tf
+++ examples/shop/frontend/infra-repo/terraform/platform-infra/main.tf
@@ -18,5 +18,5 @@ module "deploy-pipeline"{
 module "endpoint" {
     source = "git::https://github.com/ameer00/Building-Reliable-Platforms-on-GCP-with-Google-SRE.git//modules/endpoints?ref=tf_in_repo_examples"
     project_id = var.project_id
-    service_name = "${var.app_name}"
+    service_name = "shop-frontend"
 }

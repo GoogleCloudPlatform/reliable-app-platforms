a03c3a64cbb379cd9deab08794ef3a7dea4504b6
gushob21 <gushob@google.com>
2024-03-05 22:18:59 +0000

    Add application onboarding

--- modules/onboard-app/prep-app-repo.sh
+++ modules/onboard-app/prep-app-repo.sh
@@ -7,7 +7,7 @@ github_email=${4}
 github_token=${5}
 github_repo=${6}
 git clone  https://${github_user}:${github_token}@github.com/${github_org}/${github_repo} repo
-cp -r ./templates/infra-app-template/* repo
+cp -r templates/infra-app-template/* repo
 cd repo
 git config --global user.name ${github_user}
 git config --global user.email ${github_email}

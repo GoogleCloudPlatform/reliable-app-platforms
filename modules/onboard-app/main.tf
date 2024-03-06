1ebe60cd05b157498e1074d5935066f5a37f72c9
gushob21 <gushob@google.com>
2024-03-06 04:02:11 +0000

    Add application onboarding

--- modules/onboard-app/main.tf
+++ modules/onboard-app/main.tf
@@ -102,14 +102,14 @@ resource "google_cloudbuild_trigger" "deploy-infra" {
         "-c",
         <<-EOF
       export TF_VAR_project_id=${"$"}{_PROJECT_ID}
-      export TF_VAR_service_name=${"$"}${_SERVICE}
-      export TF_VAR_app_name=${_APP_NAME}
-      export TF_VAR_archetype=${_ARCHETYPE}
-      export TF_VAR_zone_index=${_ZONEINDEX}
-      export TF_VAR_region_index=${_REGION_INDEX}
-      export TF_VAR_pipeline_location=${_PIPELINE_LOCATION}
+      export TF_VAR_service_name=${"$"}{_SERVICE}
+      export TF_VAR_app_name=${"$"}{_APP_NAME}
+      export TF_VAR_archetype=${"$"}{_ARCHETYPE}
+      export TF_VAR_zone_index=${"$"}{_ZONEINDEX}
+      export TF_VAR_region_index=${"$"}{_REGION_INDEX}
+      export TF_VAR_pipeline_location=${"$"}{_PIPELINE_LOCATION}
 
-      terraform init -backend-config="bucket=${_PROJECT_ID}"
+      terraform init -backend-config="bucket=${"$"}{_PROJECT_ID}"
       terraform apply --auto-approve
 
   EOF

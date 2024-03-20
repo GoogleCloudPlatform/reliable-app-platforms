57728f1646763e62fb85260e4678dc352fb1c493
gushob21 <gushob@google.com>
2024-03-20 15:40:38 +0000

    Add application onboarding for nginx

--- modules/onboard-app/prep-infra-repo.sh
+++ modules/onboard-app/prep-infra-repo.sh
@@ -9,8 +9,8 @@ github_repo=${6}
 git clone  https://${github_user}:${github_token}@github.com/${github_org}/${github_repo} repo
 echo `ls -lrt`
 cp -r templates/infra-repo-template/* repo
-cd repo/terraform
-find . -type f  -exec  sed -i "s?^APP_NAME?${application_name}?g" {} +
+cd repo
+find terraform -type f  -exec  sed -i "s?^APP_NAME?${application_name}?g" {} +
 git config --global user.name ${github_user}
 git config --global user.email ${github_email}
 git add .

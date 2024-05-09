74a9c5998eb9209d4739c22577c49ea71e5c9df8
Steve McGhee <steve@mcgheemail.com>
2024-05-09 13:33:04 -0700

    bump google provider

--- modules/onboard-app/templates/infra-repo-template/terraform/platform-infra/providers.tf
+++ modules/onboard-app/templates/infra-repo-template/terraform/platform-infra/providers.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.80.0"
+      version = "~> 5.27"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 4.80.0"
+      version = "~> 4.27"
     }
     github = {
       source  = "hashicorp/github"

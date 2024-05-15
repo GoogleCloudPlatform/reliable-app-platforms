0de39a0ee86ecf33c55f16906dcfb09ee87269d9
Manasa Kandula <manasakandula.m@gmail.com>
2024-05-15 10:07:27 +0000

    added depends_on for mci

--- infra/terraform/gke/mci.tf
+++ infra/terraform/gke/mci.tf
@@ -8,4 +8,7 @@ resource "google_gke_hub_feature" "gke-config-mci" {
       config_membership = "projects/${var.project_id}/locations/global/memberships/${module.gke-config-cluster.name}"
     }
   }
+  depends_on = [
+    module.gke-config-cluster, module.fleet-hub
+  ]
 }
